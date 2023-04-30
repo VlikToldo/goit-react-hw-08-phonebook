207315eba003017bb03d7064aea9d3a87da8753a
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-04-30 15:08:24 +0300

    delete getUse

--- src/page/AddNumberPage/AddNumberPage.jsx
+++ src/page/AddNumberPage/AddNumberPage.jsx
@@ -1,34 +1,32 @@
-import { useSelector } from 'react-redux';
+import { useSelector} from 'react-redux';
+
 
 import ContactForm from '../../components/ContactForm/ContactcForm';
 import ContactFilter from '../../components/ContactFilter/ContactFilter';
 import ContactList from '../../components/ContactList/ContactList';
 
 import { getFilteredContacts } from '../../redux/contacts/contacts-selectors';
-import { getUser } from 'redux/auth/auth-selectors';
 
 import styles from './add-number-page.module.css';
 
 const AddNumberPage = () => {
-  const contacts = useSelector(getFilteredContacts);
-  const isContacts = Boolean(contacts.length);
+    const contacts = useSelector(getFilteredContacts);
+    const isContacts = Boolean(contacts.length);
 
-  return (
-    <div className={styles.contactBox}>
-      <div className={styles.block}>
-        <h3>Pnonebook</h3>
-        <ContactForm />
-      </div>
-      <div className={styles.block}>
-        <h3>Contacts</h3>
-        <div className={styles.listBox}>
-          <ContactFilter />
-          {isContacts && <ContactList />}
-          {!isContacts && 'You dont have a contacts'}
-        </div>
+    return <div className={styles.contactBox}>
+    <div className={styles.block}>
+      <h3>Pnonebook</h3>
+      <ContactForm />
+    </div>
+    <div className={styles.block}>
+      <h3>Contacts</h3>
+      <div className={styles.listBox}>
+        <ContactFilter />
+        {isContacts && <ContactList />}
+        {!isContacts && 'You dont have a contacts'}
       </div>
     </div>
-  );
-};
+  </div>
+}
 
 export default AddNumberPage;
